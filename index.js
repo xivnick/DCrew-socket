1cbff5e352ec1b9c1549ae3c14c2a53834bb4679
김민재 <xivnick@gmail.com>
2020-08-30 22:57:17 +0900

    응답 안하는 오류 수정

--- index.js
+++ index.js
@@ -29,6 +29,8 @@ app.post('/game/update', (req, res) => {
 
     io.sockets.in(room).emit('update', target);
     console.log(`[update] update on ${target} of ${room}`);
+
+    res.json();
 });
 
 io.on('connection', (socket) => {
@@ -40,18 +42,20 @@ io.on('connection', (socket) => {
         // set socket data
         socket.uid = uid;
         socket.name = name;
-        socket.room = room;
+        socket.room = room || 0;
 
-        axios.post(base_url + '/room/user/update',{
-            type: 'connect',
-            room_id: socket.room,
-            user_id: socket.uid,
-            x_key: secret["X_KEY"],
-        }).then((result) => {
-            console.log('[resp]/room/player: ', result.data);
-        }).catch((err) => {
-            console.log('[err]', err);
-        });
+        if(room > 0){
+            axios.post(base_url + '/room/user/update',{
+                type: 'connect',
+                room_id: socket.room,
+                user_id: socket.uid,
+                x_key: secret["X_KEY"],
+            }).then((result) => {
+                console.log('[resp]/room/player: ', result.data);
+            }).catch((err) => {
+                console.log('[err]', err);
+            });
+        }
 
         socket.join(socket.room)
         socket.broadcast.to(socket.room).emit('join', socket.name);
@@ -65,16 +69,18 @@ io.on('connection', (socket) => {
 
     socket.on('disconnect', () => {
 
-        axios.post('http://127.0.0.1/room/user/update',{
-            type: 'disconnect',
-            room_id: socket.room,
-            user_id: socket.uid,
-            x_key: secret["X_KEY"],
-        }).then((result) => {
-            console.log('[resp]/room/player: ', result.data);
-        }).catch((err) => {
-            console.log('[err]', err);
-        });
+        if(socket.room > 0){
+            axios.post('http://127.0.0.1/room/user/update',{
+                type: 'disconnect',
+                room_id: socket.room,
+                user_id: socket.uid,
+                x_key: secret["X_KEY"],
+            }).then((result) => {
+                console.log('[resp]/room/player: ', result.data);
+            }).catch((err) => {
+                console.log('[err]', err);
+            });
+        }
 
         if(socket.name) io.sockets.in(socket.room).emit('disconnect', null, socket.name);
         console.log(`[disconn] client disconnected : ${socket.name}(${socket.id})`);
