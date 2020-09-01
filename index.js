07249cc9e39d5344b07e1c47052e3984e300e1d7
김민재 <xivnick@gmail.com>
2020-09-01 22:16:18 +0900

    소켓 업데이트 삭제 / room delete시 update reload 호출

--- index.js
+++ index.js
@@ -35,21 +35,11 @@ app.post('/room/update', (req, res) => {
     res.json();
 });
 
-app.post('/socket/update', (req, res) => {
-
-    const socket_id = req.body.socket_id;
-    const target = req.body.target;
-
-    const socket = io.sockets.connected[socket_id]
-
-    socket.emit('update', target);
-    console.log(`[update] update on ${target} to ${socket.name}`);
-})
-
 app.post('/room/delete', (req, res) => {
     const room = req.body.room;
 
     settings[room] = {};
+    io.sockets.in(room).emit('update', 'reload');
     res.json();
 })
 
